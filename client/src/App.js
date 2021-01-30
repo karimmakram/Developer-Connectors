e7e45f4075c901cc177f3cbab09991c80d13ce3f
Karim <karimomakram11@gmail.com>
2021-01-30 14:40:16 +0200

    init client side with react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>App</h1>
+    </Fragment>
+  );
+}
+
+export default App;
--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -4,9 +4,12 @@ import userController from '../controllers/Cn_user'
 import auth from '../middleware/auth'
 import upload from '../middleware/upload'
 const userRouter = Router()
+
+// add user
 userRouter.post("/user", upload.single('avatar'),
     userController.Register)
 
+//login
 userRouter.post('/login', [check('email', 'include valid email').isEmail(),
 check('password', 'password is required').exists()], userController.Login)
 
@@ -14,5 +17,6 @@ userRouter.post('/userauth', auth, (req, res) => {
     res.send(req.user)
 })
 
+//get user image
 userRouter.get('/userAvatar/:id', userController.ImageUrl)
 export default userRouter
